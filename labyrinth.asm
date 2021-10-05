e3d1b2952d26bbb83919d2020154611e45065a82
Arkadiusz Wegrzyn <arek.wegrzynw@gmail.com>
2021-10-05 17:12:20 +0200

    Create labyrinth.asm

---
+++ labyrinth.asm
@@ -0,0 +1,89 @@
+# Return address at: 0x00
+# Consts:
+#    - move forward:   0x04
+#    - turn left:      0x08
+#    - turn right:     0x10
+#    - obstacle:       0x100
+#    - turning|moving: 0x600
+
+main:
+  # Wait until stops moving then return to "main2"
+  A = main2
+  D = A
+  A = 0
+  *A = D
+  A = wait
+  JMP
+main2:  
+  # Has obstacle?
+  A = 0x100
+  D = A
+  A = 0x7fff
+  D = D & *A
+
+  # Set return address to "main"
+  A = 0
+  *A = main
+  
+  # If has obstacle, turn right
+  A = turn_right
+  D ; JGT
+  
+  # If not, move forward
+  A = move
+  JMP
+
+turn_right:
+  # Command it to turn right
+  A = 0x10
+  D = A
+  A = 0x7fff
+  *A = D
+  # Set return address to continue "turn right" command
+  A = turn_right_continue
+  D = A
+  A = 0
+  *A = D
+  # Wait for the turn to finish
+  A = wait
+  JMP
+turn_right_continue:
+  # Reset "turn right" command and return to main
+  A = 0x7fff
+  *A = 0
+  A = main
+  JMP
+
+move:
+  A = 0x4
+  D = A
+  A = 0x7fff
+  *A = D
+  # Set return address to continue "move" command
+  A = move_continue
+  D = A
+  A = 0
+  *A = D
+  # Wait for the move to finish
+  A = wait
+  JMP
+move_continue:
+  # Reset "move forward" command and return to main
+  A = 0x7fff
+  *A = 0
+  A = main
+  JMP
+
+wait:
+  # Check if turning or moving is in progress
+  A = 0x7fff
+  D = *A
+  A = 0x600
+  D = D & *A
+  # Retrieve return address from 0x00
+  A = 0
+  A = *A
+  # If not turning/moving, jump back to return address
+  D ; JEQ
+  # Otherwise wait again
+  A = wait
