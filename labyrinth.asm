60c0ea2a9bfbd0e9df3144cc12e781318a5a97f6
Arkadiusz Wegrzyn <arek.wegrzynw@gmail.com>
2021-10-05 18:40:41 +0200

    Update labyrinth.asm
    Remove conditional action move/turn

--- labyrinth.asm
+++ labyrinth.asm
@@ -4,58 +4,54 @@
 #    - turn left:      0x08
 #    - turn right:     0x10
 #    - obstacle:       0x100
-#    - turning|moving: 0x600
+#    - turning|moving: 0x200|0x400 = 0x600
+pre:
+  # Save "turn right" command at "obstacle" address, trick
+  A = 0x10
+  D = A
+  A = 0x100
+  *A = D
+
+  # Save "move forward" command at 0x00 (nothing happening)
+  A = 0x04
+  D = A
+  A = 0
+  *A = D
 
 main:
-  # Wait until stops moving then return to "main2"
-  A = wait
-  JMP
-main2:  
   # Has obstacle?
   A = 0x100
   D = A
   A = 0x7fff
   D = D & *A
 
-  # Reset all move/turn commands
+  # Do what is stored under address D
+  # D can be:
+  #   0x00  - if no obstacle
+  #   0x100 - has obstacle
+  # Proper actions for those situations
+  # are hardcoded under those addresses
+  # in order to avoid additional branching.
+  #   0x00  - no obstacle, move forward = 0x04
+  #   0x100 - obstacle, turn right = 0x10
+  A = D
+  D = *A
+  A = 0x7fff
+  *A = D
   *A = 0
-  
-  # If has obstacle, turn right
-  A = turn_right
-  D ; JGT
-  
-  # If not, move forward
-  A = move
-  JMP
-
-turn_right:
-  # Command it to turn right
-  A = 0x10
-  D = A
-  A = 0x7fff
-  *A = D
-  # Wait for the turn to finish and return to main2
   A = wait
   JMP
 
-move:
-  # Command to move forward
-  A = 0x4
-  D = A
-  A = 0x7fff
-  *A = D
-  # Wait for the move to finish and return to main2
-  A = wait
-  JMP
 
 wait:
   # Check if turning or moving is in progress
   A = 0x7fff
   D = *A
   A = 0x600
-  D = D & *A
-  # If not turning/moving, jump back to main2
-  A = main2
+  D = D & A
+  # If not turning or moving, return to main
+  A = main
   D ; JEQ
   # Otherwise wait again (loop)
   A = wait
+  JMP
